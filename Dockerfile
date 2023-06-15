3c5d947f574cc15d90cfd8c12cab337192924039
David Gasquez <davidgasquez@gmail.com>
2023-06-15 14:24:45 +0000

    feat: 🎨 quarto

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM mcr.microsoft.com/devcontainers/python:3.11
 
+# Install Quarto
+RUN curl -sL $(curl https://quarto.org/docs/download/_download.json | grep -oP "(?<=\"download_url\":\s\")https.*${ARCH}\.deb") --output /tmp/quarto.deb \
+    && dpkg -i /tmp/quarto.deb \
+    && rm /tmp/quarto.deb
+
 # Copy requirements.txt to the container
 COPY requirements.txt /tmp/requirements.txt
 
---
+++ src/spacescope_api.py
@@ -0,0 +1,31 @@
+import os
+import requests
+
+SPACESCOPE_TOKEN = os.environ.get("SPACESCOPE_TOKEN")
+SPACESCOPE_API_ENDPOINT = "https://api.spacescope.io/v2/"
+
+
+def call_method(method: str, params=None):
+    endpoint = f"{SPACESCOPE_API_ENDPOINT}/{method}"
+    headers = {"Authorization": f"Bearer {SPACESCOPE_TOKEN}"}
+
+    r = requests.get(
+        endpoint,
+        headers=headers,
+        params=params,
+    )
+
+    if r.status_code != 200:
+        raise Exception(f"Error: {r.text}")
+
+    return r.json()
+
+
+def get_power_data(date=None, miner_id=None):
+    params = {"state_date": date, "miner_id": miner_id}
+
+    response = call_method(
+        "storage_provider/power", params={"state_date": date, "miner_id": miner_id}
+    )
+
+    return response
