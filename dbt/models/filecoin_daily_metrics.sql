a36a98ec8cf0b5d7dbb05c6d3b19ab5b8fdfd455
David Gasquez <davidgasquez@gmail.com>
2024-04-05 15:08:58 +0000

    feat: 🔥 add deal expiration metrics [skip ci]

--- dbt/models/filecoin_daily_metrics.sql
+++ dbt/models/filecoin_daily_metrics.sql
@@ -6,12 +6,25 @@ with date_calendar as (
 
 deal_metrics as (
     select
-        date_trunc('day', sector_start_at) as date,
+        cast(sector_start_at as date) as date,
         sum(padded_piece_size_tibs / 1024) as onboarded_data_pibs,
         sum(padded_piece_size_tibs / 1024) filter (piece_client_replication_order = 1 and piece_provider_replication_order = 1) as unique_data_onboarded_data_pibs,
+        unique_data_onboarded_data_pibs / onboarded_data_pibs as unique_data_onboarded_data_percentage,
         count(distinct deal_id) as deals,
         count(distinct client_id) as unique_deal_making_clients,
         count(distinct provider_id) as unique_deal_making_providers
+    from {{ ref('filecoin_state_market_deals') }}
+    where 1 = 1
+        and sector_start_at is not null
+    group by 1
+    order by 1
+),
+
+deal_expirations as (
+    select
+        cast(coalesce(slash_at, end_at) as date) as date,
+        count(distinct deal_id) as deal_expirations,
+        sum(padded_piece_size_tibs / 1024) as expired_data_pibs
     from {{ ref('filecoin_state_market_deals') }}
     where 1 = 1
         and sector_start_at is not null
@@ -43,12 +56,31 @@ daily_power as (
     where 1 = 1
     group by 1
     order by 1
+),
+
+new_clients as (
+    select
+        cast(first_deal_at as date) as date,
+        coalesce(count(distinct client_id), 0) as new_client_ids
+    from {{ ref('filecoin_clients') }}
+    group by 1
+    order by 1 desc
+),
+
+new_providers as (
+    select
+        cast(first_deal_at as date) as date,
+        coalesce(count(distinct provider_id), 0) as new_provider_ids
+    from {{ ref('filecoin_storage_providers') }}
+    group by 1
+    order by 1 desc
 )
 
 select
     date_calendar.day as date,
     onboarded_data_pibs,
     unique_data_onboarded_data_pibs,
+    unique_data_onboarded_data_percentage,
     data_on_active_deals_pibs,
     unique_data_on_active_deals_pibs,
     deals,
@@ -60,9 +92,16 @@ select
     raw_power_pibs,
     quality_adjusted_power_pibs,
     verified_data_power_pibs,
-    data_on_active_deals_pibs / raw_power_pibs * 100 as network_utilization
+    data_on_active_deals_pibs / raw_power_pibs as network_utilization_percentage,
+    new_client_ids,
+    new_provider_ids,
+    deal_expirations,
+    expired_data_pibs
 from date_calendar
 left join deal_metrics on date_calendar.day = deal_metrics.date
 left join users_with_active_deals on date_calendar.day = users_with_active_deals.day
 left join daily_power on date_calendar.day = daily_power.date
+left join new_clients on date_calendar.day = new_clients.date
+left join new_providers on date_calendar.day = new_providers.date
+left join deal_expirations on date_calendar.day = deal_expirations.date
 order by date_calendar.day desc
