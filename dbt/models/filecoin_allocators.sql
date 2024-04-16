b3b357b2acd099f4142f279b952bb79e708529a9
David Gasquez <davidgasquez@gmail.com>
2024-04-16 12:06:02 +0200

    chore: 🎨 create allocator models and metrics

--- dbt/models/filecoin_allocators.sql
+++ dbt/models/filecoin_allocators.sql
@@ -1,22 +1,23 @@
 with source as (
     select
-        addressId as notary_id,
-        address as notary_address,
+        addressId as allocator_id,
+        address as allocator_address,
         auditTrail as audit_trail_url,
-        nullif(trim(name), '') as notary_name,
-        orgName as notary_organization_name,
+        nullif(trim(name), '') as allocator_name,
+        orgName as allocator_organization_name,
         removed,
         initialAllowance::bigint as initial_allowance_bytes,
         initialAllowance::bigint / power(1024, 4) as initial_allowance_tibs,
         allowance::bigint as current_allowance_bytes,
         allowance::bigint / power(1024, 4) as current_allowance_tibs,
         isMultisig as is_multisig,
-        createdAtHeight as created_at_height,
+        createdAtHeight::int as created_at_height,
         to_timestamp(created_at_height * 30 + 1598306400)::timestamp as created_at,
         to_timestamp(issueCreateTimestamp)::timestamp as issue_created_at,
         verifiedClientsCount as verified_clients_count,
         receivedDatacapChange as received_datacap_change,
-        allowanceArray as allowances
+        allowanceArray as allowances,
+        case when created_at_height > 3656949 then true else false end as is_active
     from {{ source('raw_assets', 'raw_datacapstats_verifiers') }}
 )
 
--- dbt/models/filecoin_storage_providers.sql
+++ dbt/models/filecoin_storage_providers.sql
@@ -6,23 +6,36 @@ stats as (
     select
         provider_id,
         count(distinct deal_id) as total_deals,
-        count(distinct client_id) as total_unique_clients,
+        count(distinct deal_id) filter (where is_verified) as total_verified_deals,
+        count(distinct deal_id) filter (where is_active) as total_active_deals,
+        count(distinct deal_id) filter (where is_active and is_verified) as total_active_verified_deals,
+
         count(distinct piece_cid) as total_unique_piece_cids,
+        count(distinct piece_cid) filter (where is_verified) as total_verified_unique_piece_cids,
+        count(distinct piece_cid) filter (where is_active) as total_active_unique_piece_cids,
+        count(distinct piece_cid) filter (where is_active and is_verified) as total_active_verified_unique_piece_cids,
+
         sum(unpadded_piece_size_tibs) as total_data_uploaded_tibs,
-        sum(case when is_active then unpadded_piece_size_tibs else 0 end) as total_active_data_uploaded_tibs,
-        count(case when is_active then 1 else 0 end) as total_active_deals,
-        count(case when is_verified then 1 else 0 end) as total_verified_deals,
-        count(case when is_active and is_verified then 1 else 0 end) as total_active_verified_deals,
+        sum(unpadded_piece_size_tibs) filter (where is_active) as total_active_data_uploaded_tibs,
+        sum(unpadded_piece_size_tibs) filter (piece_provider_replication_order = 1) as unique_data_uploaded_tibs,
+        sum(unpadded_piece_size_tibs) filter (where is_active and piece_provider_replication_order = 1) as unique_active_data_uploaded_tibs,
+        unique_data_uploaded_tibs / sum(unpadded_piece_size_tibs) as unique_data_uploaded_ratio,
+
+        count(distinct client_id) as total_unique_clients,
+        count(distinct client_id) filter (where is_active) as total_active_unique_clients,
+        count(distinct client_id) filter (where is_active and is_verified) as total_active_verified_unique_clients,
+
         min(sector_start_at) as first_deal_at,
         min(case when is_active then sector_start_at else null end) as first_active_deal_at,
         max(sector_start_at) as last_deal_at,
         max(case when is_active then sector_start_at else null end) as last_active_deal_at,
-        sum(case when sector_start_at > current_date() - interval '30 days' then unpadded_piece_size_tibs else 0 end) as data_uploaded_tibs_30d,
-        sum(unpadded_piece_size_tibs) filter (piece_provider_replication_order = 1) as unique_data_uploaded_tibs,
-        unique_data_uploaded_tibs / sum(unpadded_piece_size_tibs) as unique_data_uploaded_percentage
+
+        sum(unpadded_piece_size_tibs) filter(where sector_start_at > current_date() - interval '30 days') as data_uploaded_tibs_30d,
+        sum(unpadded_piece_size_tibs) filter(where sector_start_at > current_date() - interval '6 months') as data_uploaded_tibs_6m,
+        sum(unpadded_piece_size_tibs) filter(where sector_start_at > current_date() - interval '1 year') as data_uploaded_tibs_1y,
     from {{ ref("filecoin_state_market_deals") }}
+    where sector_start_epoch is not null
     group by 1
-    order by 2 desc
 ),
 
 reputation_data as (
@@ -34,25 +47,23 @@ reputation_data as (
         score as score,
         rank
     from {{ source('raw_assets', 'raw_storage_providers_reputation') }}
+),
+
+power_data as (
+    select distinct
+        provider_id,
+        raw_power_pibs,
+        quality_adjusted_power_pibs,
+        verified_data_power_pibs,
+    from {{ ref("filecoin_storage_providers_power") }}
+    qualify row_number() over (partition by provider_id order by date desc) = 1
 )
 
 select
-    stats.provider_id,
-    stats.total_deals,
-    stats.total_unique_clients,
-    stats.total_unique_piece_cids,
-    stats.total_data_uploaded_tibs,
-    stats.total_active_data_uploaded_tibs,
-    stats.total_active_deals,
-    stats.total_verified_deals,
-    stats.total_active_verified_deals,
-    stats.first_deal_at,
-    stats.first_active_deal_at,
-    stats.last_deal_at,
-    stats.last_active_deal_at,
-    stats.data_uploaded_tibs_30d,
-    stats.unique_data_uploaded_tibs,
-    stats.unique_data_uploaded_percentage,
+    stats.*,
+    power_data.raw_power_pibs,
+    power_data.quality_adjusted_power_pibs,
+    power_data.verified_data_power_pibs,
     storage_provider_location.region,
     storage_provider_location.country,
     storage_provider_location.latitude,
@@ -65,3 +76,4 @@ select
 from stats
 left join storage_provider_location on stats.provider_id = storage_provider_location.provider_id
 left join reputation_data on stats.provider_id = reputation_data.provider_id
+left join power_data on stats.provider_id = power_data.provider_id
