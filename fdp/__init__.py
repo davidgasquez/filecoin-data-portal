095542f3d091e03987933309a75e25412512254e
David Gasquez <davidgasquez@gmail.com>
2024-08-09 15:01:55 +0000

    feat: ⚡️ add BigQuery resource

--- fdp/__init__.py
+++ fdp/__init__.py
@@ -1,29 +1,30 @@
 import os
 
 from dagster import EnvVar, Definitions, load_assets_from_modules
-from dagster_dbt import DbtProject, DbtCliResource
+from dagster_gcp import BigQueryResource
 from dagster_duckdb import DuckDBResource
 from dagster_duckdb_pandas import DuckDBPandasIOManager
 
 from . import resources
 from .assets import dbt, lily, other, datacap, reputation, spacescope
 
-DBT_PROJECT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/../dbt/"
 DATABASE_PATH = os.getenv(
     "DATABASE_PATH",
     os.path.dirname(os.path.abspath(__file__)) + "/../data/database.duckdb",
 )
 
-dbt_project = DbtProject(project_dir=DBT_PROJECT_DIR)
-dbt_project.prepare_if_dev()
-dbt_resource = DbtCliResource(project_dir=dbt_project, profiles_dir=DBT_PROJECT_DIR)
-
 all_assets = load_assets_from_modules(
     [other, datacap, lily, spacescope, reputation, dbt]
 )
 
+lily_bigquery = BigQueryResource(
+    project="protocol-labs-data-nexus",
+    location="us-east4",
+    gcp_credentials=EnvVar("ENCODED_GOOGLE_APPLICATION_CREDENTIALS"),
+)
+
 resources = {
-    "dbt": dbt_resource,
+    "dbt": resources.dbt_resource,
     "spacescope_api": resources.SpacescopeResource(
         SPACESCOPE_TOKEN=EnvVar("SPACESCOPE_TOKEN")
     ),
@@ -35,6 +36,7 @@ resources = {
     "duckdb": DuckDBResource(database=DATABASE_PATH),
     "io_manager": DuckDBPandasIOManager(database=DATABASE_PATH, schema="raw"),
     "dune": resources.DuneResource(DUNE_API_KEY=EnvVar("DUNE_API_KEY")),
+    "lily_bigquery": lily_bigquery,
 }
 
 defs = Definitions(assets=[*all_assets], resources=resources)
--- fdp/assets/dbt.py
+++ fdp/assets/dbt.py
@@ -1,7 +1,9 @@
 from dagster import AssetExecutionContext
 from dagster_dbt import DbtCliResource, dbt_assets
 
+from ..resources import dbt_resource
 
-@dbt_assets(manifest="dbt/target/manifest.json")
+
+@dbt_assets(manifest=dbt_resource.project_dir + "/target/manifest.json")
 def fdp_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
