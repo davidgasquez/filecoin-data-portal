8e48d57d4aadd0941a2ce0c5d696f301076093b1
David Gasquez <davidgasquez@gmail.com>
2024-09-17 09:37:46 +0200

    refactor: ♻️ Update JSON path for unnest in Filecoin SQL models [skip ci]
    Updated JSON path keys from 'allowanceArray' to 'unnest' and replaced casts for timestamps and numerics with `try_cast` in both `filecoin_allocators_datacap_allowances.sql` and `filecoin_clients_datacap_allowances.sql`.
    
    - Ensured more robust parsing with `try_cast` for potential invalid data types.
    - Made JSON path changes to align with updated data structure.

--- dbt/models/filecoin_allocators_datacap_allowances.sql
+++ dbt/models/filecoin_allocators_datacap_allowances.sql
@@ -1,16 +1,16 @@
 with source as (
     select
         addressId as allocator_id,
-        aa ->> '$.allowanceArray.id' as allowance_id,
-        aa ->> '$.allowanceArray.error' as allowance_error,
-        aa ->> '$.allowanceArray.height' as height,
-        aa ->> '$.allowanceArray.msgCID' as message_cid,
-        aa ->> '$.allowanceArray.retries' as retries,
-        aa ->> '$.allowanceArray.allowance' as allowance,
-        aa ->> '$.allowanceArray.auditTrail' as audit_trail,
-        aa ->> '$.allowanceArray.verifierId' as verifier_address_id,
-        aa ->> '$.allowanceArray.issueCreateTimestamp' as issue_created_at,
-        aa ->> '$.allowanceArray.createMessageTimestamp' as messaged_created_at
+        aa ->> '$.unnest.id' as allowance_id,
+        aa ->> '$.unnest.error' as allowance_error,
+        aa ->> '$.unnest.height' as height,
+        aa ->> '$.unnest.msgCID' as message_cid,
+        aa ->> '$.unnest.retries' as retries,
+        aa ->> '$.unnest.allowance' as allowance,
+        aa ->> '$.unnest.auditTrail' as audit_trail,
+        aa ->> '$.unnest.verifierId' as verifier_address_id,
+        aa ->> '$.unnest.issueCreateTimestamp' as issue_created_at,
+        aa ->> '$.unnest.createMessageTimestamp' as messaged_created_at
     from {{ source("raw_assets", "raw_datacapstats_verifiers") }}, unnest(allowanceArray) as aa
 )
 
@@ -19,14 +19,14 @@ select
     allocator_id,
     allowance_error,
     height::numeric as height,
-    to_timestamp(height::numeric * 30 + 1598306400)::timestamp as height_at,
+    to_timestamp(try_cast(height as numeric) * 30 + 1598306400)::timestamp as height_at,
     message_cid,
     retries,
     allowance::bigint as allowance_bytes,
     allowance::bigint / power(1024, 4) as allowance_tibs,
     audit_trail,
     verifier_address_id,
-    to_timestamp(issue_created_at::numeric) as issue_created_at,
+    to_timestamp(try_cast(issue_created_at as numeric)) as issue_created_at,
     to_timestamp(messaged_created_at::numeric) as messaged_created_at
 from source
 qualify row_number() over (partition by message_cid, height order by height desc) = 1
