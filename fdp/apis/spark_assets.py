23550f1f2a7c45fc74321628da8a609764a329be
David Gasquez <davidgasquez@gmail.com>
2025-01-17 10:50:38 +0100

    fix(spark): 🔧 fix indentation and streamline db connection handling

--- fdp/apis/spark_assets.py
+++ fdp/apis/spark_assets.py
@@ -28,101 +28,99 @@ def raw_spark_retrievals_onchain_data(
             context.log.error(f"Error fetching max index from duckdb. {e}")
             max_index = 0
 
-    rpc_url = "https://filecoin.chainup.net/rpc/v1"
-    abi_url = "https://raw.githubusercontent.com/filecoin-station/spark-rsr-contract/refs/heads/main/out/SparkRsr.sol/SparkRsr.json"
-    contract_address = "0x620bfc5AdE7eeEE90034B05DC9Bb5b540336ff90"
+        rpc_url = "https://filecoin.chainup.net/rpc/v1"
+        abi_url = "https://raw.githubusercontent.com/filecoin-station/spark-rsr-contract/refs/heads/main/out/SparkRsr.sol/SparkRsr.json"
+        contract_address = "0x620bfc5AdE7eeEE90034B05DC9Bb5b540336ff90"
 
-    response = httpx_api.get(abi_url)
-    abi_json = response.json()
-    abi = abi_json["abi"]
+        response = httpx_api.get(abi_url)
+        abi_json = response.json()
+        abi = abi_json["abi"]
 
-    provider = Web3(Web3.HTTPProvider(rpc_url))
+        provider = Web3(Web3.HTTPProvider(rpc_url))
 
-    contract_address = Web3.to_checksum_address(contract_address)
-    contract = provider.eth.contract(address=contract_address, abi=abi)
+        contract_address = Web3.to_checksum_address(contract_address)
+        contract = provider.eth.contract(address=contract_address, abi=abi)
 
-    results_cids = []
-    i = max_index
+        results_cids = []
+        i = max_index
 
-    while True:
-        try:
-            cid_info = {}
-            cid = contract.functions.providerRetrievalResultStats(i).call()
-            cid_info["cid"] = cid
-            cid_info["index"] = i
-            results_cids.append(cid_info)
-            i += 1
-        except Exception as e:
-            context.log.error(f"Error fetching retrieval result stats. {e}")
-            break
+        while True:
+            try:
+                cid_info = {}
+                cid = contract.functions.providerRetrievalResultStats(i).call()
+                cid_info["cid"] = cid
+                cid_info["index"] = i
+                results_cids.append(cid_info)
+                i += 1
+            except Exception as e:
+                context.log.error(f"Error fetching retrieval result stats. {e}")
+                break
 
-    context.log.info(f"Found {len(results_cids)} commitments")
+        context.log.info(f"Found {len(results_cids)} commitments")
 
-    data = []
+        data = []
 
-    for cid_info in results_cids:
-        cid = cid_info["cid"]
+        for cid_info in results_cids:
+            cid = cid_info["cid"]
 
-        context.log.info(f"Fetching CAR for CID {cid}, index {cid_info['index']}")
+            context.log.info(f"Fetching CAR for CID {cid}, index {cid_info['index']}")
 
-        url = f"https://{cid}.ipfs.w3s.link/?format=car"
+            url = f"https://{cid}.ipfs.w3s.link/?format=car"
 
-        try:
-            response = httpx_api.get(url, timeout=300)
-        except Exception as e:
-            context.log.error(
-                f"Error fetching retrieval result stats for CID {cid}. {e}"
-            )
-            continue
+            try:
+                response = httpx_api.get(url, timeout=300)
+            except Exception as e:
+                context.log.error(
+                    f"Error fetching retrieval result stats for CID {cid}. {e}"
+                )
+                continue
 
-        context.log.info(f"CAR response: {response.status_code}")
+            context.log.info(f"CAR response: {response.status_code}")
 
-        if response.status_code != 200:
-            context.log.error(
-                f"Error fetching retrieval result stats for CID {cid}. Status code: {response.status_code}. Response: {response.text}"
-            )
-            continue
+            if response.status_code != 200:
+                context.log.error(
+                    f"Error fetching retrieval result stats for CID {cid}. Status code: {response.status_code}. Response: {response.text}"
+                )
+                continue
 
-        try:
-            _, blocks = read_car(response.content, validate=True)
-        except Exception as e:
-            context.log.error(f"Error reading CAR for CID {cid}. {e}")
-            continue
+            try:
+                _, blocks = read_car(response.content, validate=True)
+            except Exception as e:
+                context.log.error(f"Error reading CAR for CID {cid}. {e}")
+                continue
 
-        main_block = blocks[0]
+            main_block = blocks[0]
 
-        json_data = decode(main_block.data)
-        json_data["retrieval_result_stats_cid"] = str(cid)  # type: ignore
-        json_data["index"] = cid_info["index"]  # type: ignore
+            json_data = decode(main_block.data)
+            json_data["retrieval_result_stats_cid"] = str(cid)  # type: ignore
+            json_data["index"] = cid_info["index"]  # type: ignore
 
-        if "meta" in json_data:  # type: ignore
-            del json_data["meta"]  # type: ignore
+            if "meta" in json_data:  # type: ignore
+                del json_data["meta"]  # type: ignore
 
-        data.append(json_data)
+            data.append(json_data)
 
-    context.log.info(f"Downloaded and parsed {len(data)} Spark retrievals stats CARs")
-
-    flattened_data = [
-        {
-            **stats,
-            "date": record["date"],
-            "cid": record["retrieval_result_stats_cid"],
-            "index": record["index"],
-        }
-        for record in data
-        for stats in record["providerRetrievalResultStats"]
-    ]
-
-    df = pl.DataFrame(flattened_data).rename({"providerId": "provider_id"})
-
-    with duckdb.get_connection() as con:
-        con.execute(
-            f"""
-            insert into raw.{asset_name}
-            select * from df
-            """
+        context.log.info(
+            f"Downloaded and parsed {len(data)} Spark retrievals stats CARs"
         )
 
-    context.log.info(f"Persisted {df.height} rows to raw.{asset_name}")
+        flattened_data = [
+            {
+                **stats,
+                "date": record["date"],
+                "cid": record["retrieval_result_stats_cid"],
+                "index": record["index"],
+            }
+            for record in data
+            for stats in record["providerRetrievalResultStats"]
+        ]
+
+        df = pl.DataFrame(flattened_data).rename({"providerId": "provider_id"})
+
+        context.log.info(f"Persisting {df.height} rows to raw.{asset_name}")
+
+        con.sql(f"insert into raw.{asset_name} select * from df")
+
+        context.log.info(f"Persisted {df.height} rows to raw.{asset_name}")
 
     return dg.MaterializeResult(metadata={"dagster/row_count": df.height})
