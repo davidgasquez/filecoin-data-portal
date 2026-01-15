13b0660cba1d105f6af983ab77b6e2489d85e1bc
David Gasquez <davidgasquez@gmail.com>
2026-01-15 20:34:58 +0100

    🧹 remove Spark assets and deps

--- fdp/apis/definitions.py
+++ fdp/apis/definitions.py
@@ -1,16 +1,13 @@
 import dagster as dg
 
-from fdp.apis import assets, spark_assets
+from fdp.apis import assets
 from fdp.resources import HttpClientResource
 
-api_assets = dg.load_assets_from_modules([assets, spark_assets])
+api_assets = dg.load_assets_from_modules([assets])
 
 definitions = dg.Definitions(
     assets=api_assets,
     resources={
         "httpx_api": HttpClientResource(retries=5, timeout=45),
-        "httpx_filspark": HttpClientResource(
-            bearer_token=dg.EnvVar("SPARK_API_BEARER_TOKEN")
-        ),
     },
 )
