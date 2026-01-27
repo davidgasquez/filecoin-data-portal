8d8bb4cad999229b8b968b9834914c59c4c37999
David Gasquez <davidgasquez@gmail.com>
2026-01-27 11:14:24 +0100

    ✨ Migrate OSO asset to API resource (#207)

--- fdp/bigquery/definitions.py
+++ fdp/bigquery/definitions.py
@@ -1,9 +1,9 @@
 import dagster as dg
 
-from fdp.bigquery import assets_lily, assets_oso
+from fdp.bigquery import assets_lily
 from fdp.bigquery.resources import fdp_bigquery, lily_bigquery
 
-assets = dg.load_assets_from_modules([assets_oso, assets_lily])
+assets = dg.load_assets_from_modules([assets_lily])
 
 definitions = dg.Definitions(
     assets=assets,
