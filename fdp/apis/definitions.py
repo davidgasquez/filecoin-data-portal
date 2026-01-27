c7ed71591388e502c35183dda3e8c53d86363117
David Gasquez <davidgasquez@gmail.com>
2026-01-27 11:01:49 +0100

    ✨ Migrate OSO asset to API resource

--- fdp/apis/definitions.py
+++ fdp/apis/definitions.py
@@ -1,13 +1,15 @@
 import dagster as dg
 
-from fdp.apis import assets, fevm, goldsky
+from fdp.apis import assets, fevm, goldsky, oso
+from fdp.apis.resources import OsoResource
 from fdp.resources import HttpClientResource
 
-api_assets = dg.load_assets_from_modules([assets, fevm, goldsky])
+api_assets = dg.load_assets_from_modules([assets, fevm, goldsky, oso])
 
 definitions = dg.Definitions(
     assets=api_assets,
     resources={
         "httpx_api": HttpClientResource(retries=5, timeout=45),
+        "oso_api": OsoResource(OSO_API_KEY=dg.EnvVar("OSO_API_KEY")),
     },
 )
