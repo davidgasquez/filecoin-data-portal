acb0a33971a75eb75bdb4f11fd43d795ef3ccecd
David Gasquez <davidgasquez@gmail.com>
2026-04-28 19:00:54 +0200

    ✨ Add network burn metrics

--- assets/model/daily/network_activity.sql
+++ assets/model/daily/network_activity.sql
@@ -7,6 +7,8 @@
 -- asset.column = transactions | Onchain transactions.
 -- asset.column = total_value_fil | FIL transferred by top-level messages.
 -- asset.column = total_gas_fee_fil | FIL paid in gas fees.
+-- asset.column = base_fee_burn_fil | FIL burned by message base fees.
+-- asset.column = message_burn_fil | FIL burned by message execution.
 -- asset.column = total_value_flow_fil | FIL value transferred plus gas fees.
 
 -- asset.not_null = date
@@ -14,6 +16,8 @@
 -- asset.not_null = transactions
 -- asset.not_null = total_value_fil
 -- asset.not_null = total_gas_fee_fil
+-- asset.not_null = base_fee_burn_fil
+-- asset.not_null = message_burn_fil
 -- asset.not_null = total_value_flow_fil
 -- asset.unique = date
 
@@ -23,6 +27,8 @@ select
     sum(transactions) as transactions,
     cast(sum(total_value_fil) as double) as total_value_fil,
     cast(sum(total_gas_fee_fil) as double) as total_gas_fee_fil,
+    cast(sum(base_fee_burn_fil) as double) as base_fee_burn_fil,
+    cast(sum(message_burn_fil) as double) as message_burn_fil,
     cast(sum(total_value_fil) + sum(total_gas_fee_fil) as double)
         as total_value_flow_fil
 from raw.daily_network_activity_by_method
