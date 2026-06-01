559ced3cb98822f83c91cdcbe246a5a01f5a54a2
David Gasquez <davidgasquez@gmail.com>
2026-06-01 10:07:16 +0200

    💚 Reduce sector lifecycle BigQuery scan

--- assets/raw/daily/sector_lifecycle.sql
+++ assets/raw/daily/sector_lifecycle.sql
@@ -1,59 +1,19 @@
 -- asset.description = Daily Filecoin sector lifecycle bytes from sector events.
--- asset.resource = bigquery.lily
+-- asset.depends = model.storage_provider_sector_lifecycle_daily
 
 -- asset.not_null = date
 -- asset.unique = date
 
-with miner_sizes as (
-    select
-        miner_id,
-        max(cast(sector_size as int64)) as sector_size
-    from `miner_infos`
-    where cast(sector_size as int64) > 0
-    group by 1
-)
 select
-    date(timestamp_seconds((e.height * 30) + 1598306400)) as date,
-    sum(
-        case
-            when e.event in ('SECTOR_ADDED', 'COMMIT_CAPACITY_ADDED')
-                then ms.sector_size
-            else 0
-        end
-    ) as onboarded_bytes,
-    countif(e.event in ('SECTOR_ADDED', 'COMMIT_CAPACITY_ADDED'))
-        as onboarded_sectors,
-    sum(
-        case
-            when e.event = 'SECTOR_TERMINATED' then ms.sector_size
-            else 0
-        end
-    ) as terminated_bytes,
-    countif(e.event = 'SECTOR_TERMINATED') as terminated_sectors,
-    sum(
-        case
-            when e.event = 'SECTOR_EXPIRED' then ms.sector_size
-            else 0
-        end
-    ) as expired_bytes,
-    countif(e.event = 'SECTOR_EXPIRED') as expired_sectors,
-    sum(
-        case
-            when e.event in ('SECTOR_TERMINATED', 'SECTOR_EXPIRED')
-                then ms.sector_size
-            else 0
-        end
-    ) as removed_bytes,
-    countif(e.event in ('SECTOR_TERMINATED', 'SECTOR_EXPIRED'))
-        as removed_sectors
-from `miner_sector_events` as e
-join miner_sizes as ms
-    using (miner_id)
-where e.event in (
-    'SECTOR_ADDED',
-    'COMMIT_CAPACITY_ADDED',
-    'SECTOR_TERMINATED',
-    'SECTOR_EXPIRED'
-)
+    date,
+    cast(sum(onboarded_tibs) * pow(1024, 4) as hugeint) as onboarded_bytes,
+    sum(onboarded_sectors) as onboarded_sectors,
+    cast(sum(terminated_tibs) * pow(1024, 4) as hugeint) as terminated_bytes,
+    sum(terminated_sectors) as terminated_sectors,
+    cast(sum(expired_tibs) * pow(1024, 4) as hugeint) as expired_bytes,
+    sum(expired_sectors) as expired_sectors,
+    cast(sum(removed_tibs) * pow(1024, 4) as hugeint) as removed_bytes,
+    sum(removed_sectors) as removed_sectors
+from model.storage_provider_sector_lifecycle_daily
 group by 1
 order by 1
