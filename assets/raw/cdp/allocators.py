7793c68db16f32aa29292d9deed40392a0125af3
David Gasquez <davidgasquez@gmail.com>
2026-09-02 20:10:15 +0200

    🐛 Increase CDP allocators timeout

--- assets/raw/cdp/allocators.py
+++ assets/raw/cdp/allocators.py
@@ -111,7 +111,7 @@ COLUMNS = (
 
 
 def allocators() -> pl.DataFrame:
-    data = httpx.get(URL, follow_redirects=True, timeout=30).raise_for_status().json()
+    data = httpx.get(URL, follow_redirects=True, timeout=60).raise_for_status().json()
     return (
         pl
         .DataFrame(data["data"], infer_schema_length=None, strict=False)
